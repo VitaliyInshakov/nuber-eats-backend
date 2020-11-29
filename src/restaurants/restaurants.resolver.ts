99109497dfc4664920d075e954b6c8780f6a54d9
Vitaliy Inshakov <michael8913@gmail.com>
2020-11-29 15:37:03 +0200

    remove prettier config, added entity, dto, validation

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,18 @@
-import { Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
+import { Restaurant } from "./entities/restaurants.entity";
+import { CreateRestaurantDto } from "./dto/restaurant.dto";
 
-@Resolver()
+@Resolver(of => Restaurant)
 export class RestaurantsResolver {
-    @Query(() => Boolean)
-    isPizzaGood() {
+    @Query(returns => [Restaurant])
+    restaurants(): Restaurant[] {
+       return [];
+    }
+
+    @Mutation(returns => Boolean)
+    createRestaurant(
+        @Args() createRestaurantDto: CreateRestaurantDto
+    ): boolean {
         return true;
     }
 }
