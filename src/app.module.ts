f256e13f4564fdcc61d9edcf1a47fe0f7989c471
Vitaliy Inshakov <michael8913@gmail.com>
2020-11-30 23:32:08 +0200

    configuring ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,36 @@
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
+import * as Joi from "joi";
 import { join } from "path";
 
 import { RestaurantsModule } from "./restaurants/restaurants.module";
 
 @Module({
     imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: ".env",
+            validationSchema: Joi.object({
+                NODE_ENV: Joi.string().valid("development", "production").required(),
+                DB_HOST: Joi.string().required(),
+                DB_PORT: Joi.string().required(),
+                DB_USERNAME: Joi.string().required(),
+                DB_PASSWORD: Joi.string().required(),
+                DB_NAME: Joi.string().required(),
+            }),
+        }),
         GraphQLModule.forRoot({
             autoSchemaFile: join(process.cwd(), "src/schema.gql"),
         }),
         TypeOrmModule.forRoot({
             type: "postgres",
-            host: "localhost",
-            port: 5432,
-            username: "postgres",
-            password: "postgres",
-            database: "nuber-eats",
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT,
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
             synchronize: true,
             logging: true,
         }),
