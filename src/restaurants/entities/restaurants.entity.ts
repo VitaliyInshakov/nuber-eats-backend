0bd63dcd8f1115fc1f915f88b5d0e6ba85ed1ed0
Vitaliy Inshakov <michael8913@gmail.com>
2020-12-01 23:38:35 +0200

    Optional Types and Columns

--- src/restaurants/entities/restaurants.entity.ts
+++ src/restaurants/entities/restaurants.entity.ts
@@ -1,5 +1,6 @@
 import { Field, ObjectType } from "@nestjs/graphql";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { IsBoolean, IsOptional, IsString } from "class-validator";
 
 @ObjectType()
 @Entity()
@@ -10,21 +11,27 @@ export class Restaurant {
 
     @Field(type => String)
     @Column()
+    @IsString()
     name: string;
 
-    @Field(type => Boolean)
-    @Column()
+    @Field(type => Boolean, { defaultValue: true })
+    @Column({ default: true })
+    @IsBoolean()
+    @IsOptional()
     isVegan: boolean;
 
     @Field(type => String)
     @Column()
+    @IsString()
     address: string;
 
     @Field(type => String)
     @Column()
+    @IsString()
     ownerName: string;
 
     @Field(type => String)
     @Column()
+    @IsString()
     category: string;
 }
