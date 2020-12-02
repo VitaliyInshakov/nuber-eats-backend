b956eb5cb15f6fee3cff56b9020f2f3b3cf7af20
Vitaliy Inshakov <michael8913@gmail.com>
2020-12-02 09:28:50 +0200

    update restaurant

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,7 +1,9 @@
 import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
-import { Restaurant } from "./entities/restaurants.entity";
-import { CreateRestaurantDto } from "./dto/restaurant.dto";
+
 import { RestaurantService } from "./restaurants.service";
+import { Restaurant } from "./entities/restaurants.entity";
+import { CreateRestaurantDto } from "./dto/create-restaurant.dto";
+import { UpdateRestaurantDto } from "./dto/update-restaurant.dto";
 
 @Resolver(of => Restaurant)
 export class RestaurantsResolver {
@@ -23,5 +25,16 @@ export class RestaurantsResolver {
            console.log(e);
            return false;
        }
+    }
+
+    @Mutation(returns => Boolean)
+    async updateRestaurant(@Args("input") updateRestaurantDto: UpdateRestaurantDto): Promise<boolean> {
+        try {
+            await this.restaurantService.updateRestaurant(updateRestaurantDto);
+            return true;
+        } catch (e) {
+            console.log(e);
+            return false;
+        }
     }
 }
