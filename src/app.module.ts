f3612e5b967299283f29dc83172869897b4ef9ad
Vitaliy Inshakov <michael8913@gmail.com>
2020-12-04 09:13:16 +0200

    create User model, resolver and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,9 @@ import { ConfigModule } from "@nestjs/config";
 import * as Joi from "joi";
 import { join } from "path";
 
-import { RestaurantsModule } from "./restaurants/restaurants.module";
-import { Restaurant } from "./restaurants/entities/restaurants.entity";
+import { UsersModule } from "./users/users.module";
+import { CommonModule } from "./common/common.module";
+import { User } from "./users/entities/user.entity";
 
 @Module({
     imports: [
@@ -34,9 +35,10 @@ import { Restaurant } from "./restaurants/entities/restaurants.entity";
             database: process.env.DB_NAME,
             synchronize: process.env.NODE_ENV !== "production",
             logging: true,
-            entities: [Restaurant],
+            entities: [User],
         }),
-        RestaurantsModule,
+        UsersModule,
+        CommonModule,
     ],
     controllers: [],
     providers: [],
