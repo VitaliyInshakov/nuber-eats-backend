99b104c66b0f9c7127c95bdd66a70deed56fb50e
Vitaliy Inshakov <michael8913@gmail.com>
2020-12-04 09:28:10 +0200

    account mutation

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,17 +1,23 @@
 import { Column, Entity } from "typeorm";
+import { Field, InputType, ObjectType } from "@nestjs/graphql";
 
 import { CoreEntity } from "src/common/entitites/core.entity";
 
 type UserRole = "client" | "owner" | "delivery";
 
+@InputType({ isAbstract: true })
+@ObjectType()
 @Entity()
 export class User extends CoreEntity {
+    @Field(type => String)
     @Column()
     email: string;
 
+    @Field(type => String)
     @Column()
     password: string;
 
+    @Field(type => String)
     @Column()
     role: UserRole;
 }
--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,8 +1,14 @@
-import { Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Resolver } from "@nestjs/graphql";
 import { User } from "./entities/user.entity";
 import { UsersService } from "./users.service";
+import { CreateAccountDto, CreateAccountOutput } from "./dto/create-account.dto";
 
 @Resolver(of => User)
 export class UsersResolver {
     constructor(private readonly usersService: UsersService) {}
+
+    @Mutation(returns => CreateAccountOutput)
+    createAccount(@Args("input") createAccountDto: CreateAccountDto) {
+
+    }
 }
