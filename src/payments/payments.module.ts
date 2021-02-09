9eb43311b4cb2b816e55a144f8da9c826dd377d3
Vitaliy Inshakov <michael8913@gmail.com>
2021-02-09 22:20:32 +0200

    fix error with payment module

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
+import { Restaurant } from "src/restaurants/entities/restaurants.entity";
 import { PaymentService } from "./payments.service";
 import { PaymentResolver } from "./payments.resolver";
 import { Payment } from "./entities/payment.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Payment])],
+    imports: [TypeOrmModule.forFeature([Payment, Restaurant])],
     providers: [PaymentService, PaymentResolver],
 })
 export class PaymentsModule {}
